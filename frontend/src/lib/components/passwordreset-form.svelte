0eea59813b9b0efac403f44ba55daebddaa00aac
WW <ww@wwology.tech>
2026-01-23 07:02:52 +0000

    add the 3 authentication pages

---
+++ frontend/src/lib/components/passwordreset-form.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import { cn } from "$lib/utils.js";
+	import { Button } from "$lib/components/ui/button/index.js";
+	import { Input } from "$lib/components/ui/input/index.js";
+	import * as Card from "$lib/components/ui/card/index.js";
+	import * as Field from "$lib/components/ui/field/index.js";
+	import type { HTMLAttributes } from "svelte/elements";
+
+	let { class: className, ...restProps }: HTMLAttributes<HTMLDivElement> = $props();
+</script>
+
+<div class={cn("flex flex-col gap-6 md:min-h-112.5", className)} {...restProps}>
+	<Card.Root class="flex-1 overflow-hidden p-0">
+		<Card.Content class="grid flex-1 p-0 md:grid-cols-2">
+			<form class="flex flex-col items-center justify-center p-6 md:p-8">
+				<Field.Group>
+					<Field.Field class="items-center text-center">
+						<h1 class="text-2xl font-bold">Enter your email</h1>
+						<p class="text-muted-foreground text-sm text-balance">
+							We will send a 6-digit code to your email
+						</p>
+					</Field.Field>
+					<Field.Field>
+						<Field.Label for="email" class="sr-only">Email</Field.Label>
+                        <Input type="email" placeholder="example@gmail.com" required />
+					</Field.Field>
+					<Field.Field>
+						<Button type="submit">Verify</Button>
+						<Field.Description class="text-center">
+							Didn't receive the code? <a href="#/">Resend</a>
+						</Field.Description>
+					</Field.Field>
+				</Field.Group>
+			</form>
+			<div class="bg-muted relative hidden md:block">
+				<img
+					src="/placeholder.svg"
+					alt=""
+					class="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+				/>
+			</div>
+		</Card.Content>
+	</Card.Root>
+	<Field.Description class="text-center">
+		By clicking continue, you agree to our <a href="#/">Terms of Service</a>
+		and <a href="#/">Privacy Policy</a>.
+	</Field.Description>
+</div>
---
+++ frontend/src/routes/login/resetpassword/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import OTPForm from "$lib/components/otp-form.svelte";
+	import PasswordresetForm from "$lib/components/passwordreset-form.svelte";
+
+	let codeSent = $state(false);
+</script>
+
+<div class="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+	<div class="w-full max-w-sm md:max-w-3xl">
+		{#if codeSent}
+			<OTPForm />
+		{:else}
+			<PasswordresetForm />
+		{/if}
+	</div>
+</div>
