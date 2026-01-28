60f03f630488b78d1d466af25d39efc00148bfe4
WW <ww@wwology.tech>
2026-01-28 02:20:22 +0000

    implement login functionality with form handling and routing

--- angular-frontend/src/app/pages/login/login-page/login-page.ts
+++ angular-frontend/src/app/pages/login/login-page/login-page.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { email, form, FormField, required } from '@angular/forms/signals';
+import { CardModule } from "primeng/card";
+import { InputGroupModule } from "primeng/inputgroup";
+import { InputGroupAddonModule } from "primeng/inputgroupaddon";
+import { InputTextModule } from "primeng/inputtext";
+import { AuthService } from '../../../features/auth/auth.service';
+
+interface LoginData {
+  email: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [CardModule, FormField, InputGroupModule, InputGroupAddonModule, InputTextModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
 export class LoginPage {
+  private authService = inject(AuthService);
+  loginModel = signal<LoginData>({
+    email: '',
+    password: '',
+  })
 
+  loginForm = form(this.loginModel);
+
+  async onSubmit()  {
+    try {
+      const formData = this.loginModel();
+      await this.authService.signInWithEmailAndPassword(formData.email, formData.password);
+    } catch (error) {
+      console.error('Login failed', error);}
+  }
 }
