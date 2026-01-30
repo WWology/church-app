f6f8412e57cf09fe0daeb47a3f5ceec32d5e2589
WW <ww@wwology.tech>
2026-01-30 17:34:08 +0000

    login-page form done

--- angular-frontend/src/app/core/auth/pages/login/login-page.ts
+++ angular-frontend/src/app/core/auth/pages/login/login-page.ts
@@ -1,10 +1,20 @@
 import { Component, inject, signal } from '@angular/core';
 import { email, form, FormField, minLength, required } from '@angular/forms/signals';
-import { CardModule } from "primeng/card";
-import { InputGroupModule } from "primeng/inputgroup";
-import { InputGroupAddonModule } from "primeng/inputgroupaddon";
-import { InputTextModule } from "primeng/inputtext";
+import { RouterLink } from '@angular/router';
+
+import { MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { DividerModule } from 'primeng/divider';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextModule } from 'primeng/inputtext';
+import { MessageModule } from 'primeng/message';
+import { ToastModule } from 'primeng/toast';
+
 import { AuthService } from '../../auth.service';
+import { sign } from 'crypto';
 
 interface LoginData {
   email: string;
@@ -13,16 +23,33 @@ interface LoginData {
 
 @Component({
   selector: 'app-login-page',
-  imports: [CardModule, FormField, InputGroupModule, InputGroupAddonModule, InputTextModule],
+  imports: [
+    ButtonModule,
+    CardModule,
+    DividerModule,
+    FloatLabelModule,
+    FormField,
+    InputGroupAddonModule,
+    InputGroupModule,
+    InputTextModule,
+    MessageModule,
+    RouterLink,
+    ToastModule,
+  ],
+  providers: [MessageService],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
 export class LoginPage {
   private authService = inject(AuthService);
+  private messageService = inject(MessageService);
+
+  loading = signal(false);
+
   loginModel = signal<LoginData>({
     email: '',
     password: '',
-  })
+  });
 
   loginForm = form(this.loginModel, (fieldPath) => {
     required(fieldPath.email, { message: 'Email is required' });
@@ -32,11 +59,45 @@ export class LoginPage {
     minLength(fieldPath.password, 6, { message: 'Password must be at least 6 characters long' });
   });
 
-  async onSubmit()  {
+  async onSubmit(event: Event) {
+    event.preventDefault();
+    this.loading.set(true);
+
+    if (this.loginForm().pending()) {
+      this.messageService.add({
+        severity: 'info',
+        summary: 'Validation Pending',
+        detail: 'Please wait for validation to complete.',
+      });
+      return;
+    }
+
+    if (this.loginForm().invalid()) {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Validation Error',
+        detail: 'Please correct the errors in the form before submitting.',
+      });
+      return;
+    }
+
     try {
       const formData = this.loginModel();
-      await this.authService.signInWithEmailAndPassword(formData.email, formData.password);
+      const { error } = await this.authService.signInWithEmailAndPassword(
+        formData.email,
+        formData.password,
+      );
+      if (error) throw error;
     } catch (error) {
-      console.error('Login failed', error);}
+      if (error instanceof Error) {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Login Failed',
+          detail: error.message,
+        });
+      }
+    } finally {
+      this.loading.set(false);
+    }
   }
 }
